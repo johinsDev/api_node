78944e98bbbcefc9376be32760f5c4b6f5b0ad10
johinsDev <johinsDev@gmail.com>
2017-03-04 06:21:09 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var cors = require('cors')
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var config = require('./lib/config');
+
+var app = express();
+
+mongoose.createConnection(config.database);
+//mongoose.connect(config.database);
+
+var auth_middle = require('./lib/middleware/auth')
+var auth = require('./routes/auth')
+var index = require('./routes/index')
+var movie = require('./routes/movie')
+var user = require('./routes/user')
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({origin: 'http://www.google.com'}))
+
+//  Rutas inseguras
+app.use('/', index)
+app.use('/auth', auth)
+app.use('/user', user)
+
+//  Middleware
+app.use(auth_middle)
+
+//  Rutas seguras
+app.use('/movie', movie)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+});
+
+
+module.exports = app;
